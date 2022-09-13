93b4d1e1162a42b9f449e9d2de1a97b33eab6106
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-09-12 22:27:00 -0300

    login

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,6 +3,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { authGoogleContex } from "../contexts/authGoogle";
 export default function PrivateRouters() {
     const {singInGoogle,signed} = useContext(authGoogleContex); 
-   
+  
   return signed ? <Outlet/> : <Navigate to='/' /> ;
 }
