2957cd6ebf837b4c91f140e1045b6c4dda12206a
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-09-25 15:04:18 -0300

    upgrade

--- src/home/PhotosItem.js
+++ src/home/PhotosItem.js
@@ -1,13 +1,21 @@
-
-
+import { ContainerHomeCenter } from "../containr/container1"
+import Card from 'react-bootstrap/Card';
 function PhotoItens(url, name){
     console.log(url)
     return(
         
-        <div>
-                <img src={url.url} title={name.name} />
-                
-
-        </div>
+        <ContainerHomeCenter style={{display:"flex"}}>
+        <Card style={{ width: '18rem', backgroundColor: "#ffff", margin: 10 }}>
+            <Card.Img style={{ width: '10rem' }} variant="top" src={url.url} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </Card.Text>
+                <button variant="primary">Go somewhere</button>
+            </Card.Body>
+        </Card>
+    </ContainerHomeCenter>
     )
 }export default PhotoItens
