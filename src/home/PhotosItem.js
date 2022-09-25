9de1be9e5d1ba364ebfc7a72fbd58beb080b102a
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-09-25 15:15:10 -0300

    Update PhotosItem.js

--- src/home/PhotosItem.js
+++ src/home/PhotosItem.js
@@ -4,19 +4,10 @@ function PhotoItens(url, name){
     console.log(url)
     return(
         
-        <ContainerHomeCenter style={{display:"flex"}}>
-        <Card style={{ width: '18rem', backgroundColor: "#ffff", margin: 10 }}>
-            <Card.Img style={{ width: '10rem' }} variant="top" src={url.url} />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <button variant="primary">Go somewhere</button>
-            </Card.Body>
-        </Card>
-    </ContainerHomeCenter>
-    
+        <ContainerHomeCenter style={{}}>
+            <img style={{margin:10, width:"10rem",borderRadius:"10px" }} src={url.url} alt={name.name} />
+
+         </ContainerHomeCenter>
+
     )
 }export default PhotoItens
