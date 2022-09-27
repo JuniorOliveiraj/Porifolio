1368cb14db06eb78af80ee8bf2a253647de95d28
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-09-27 18:10:32 -0300

    Update vh1.jsx

--- src/home/vh/vh1.jsx
+++ src/home/vh/vh1.jsx
@@ -1,8 +1,16 @@
 import { ContainerHomeCenter } from "../../containr/container1"
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 function Vhone() {
-   return(
-    <ContainerHomeCenter>
-        
-    </ContainerHomeCenter>
+   return ( 
+      <ContainerHomeCenter>
+         <React.Fragment>
+            <Container maxWidth="sm">
+               <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+            </Container>
+         </React.Fragment>
+      </ContainerHomeCenter>
    )
-}export default Vhone
+} export default Vhone
