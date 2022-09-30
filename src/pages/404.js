c4adf5d8b9a1d6f661b6b5b1c55ad1103c8b1aa8
junior de oliveira belem <junioroliveira.belem@gmail.com>
2022-09-29 22:46:11 -0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,20 +2,11 @@ import { ContainerHomeCenter } from "../containr/container1"
 import { Link } from "react-router-dom"
 import '../css/404.css'
 function NotFalPage() {
-
-
-
     return (
-
-
         <section className="container404">
-            
                 <div className="D404 D404_Text" title="404">404</div>
                 <div className="D404 D44_button" title="404" style={{marginTop: "-150px"}}><Link to='/'><button className="goHome"  > GO HOME</button></Link></div>
-          
         </section>
-
-
 
     )
 }
